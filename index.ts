a36bd89b44d1d1590a451179644f134b2f591670
Flystar-Yash <110655152+Flystar-Yash@users.noreply.github.com>
2024-12-10 00:42:23 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,22 +1,25 @@
 import * as gcp from "@pulumi/gcp";
 
-// Create Pub/Sub Topic
-const pubsubTopic = new gcp.pubsub.Topic("rate-limit-exceeded");
+// Create Pub/Sub topic
+const pubSubTopic = new gcp.pubsub.Topic("button-click-topic");
 
-// Create Cloud Run Service
-const cloudRunService = new gcp.cloudrun.Service("express-cloud-app", {
+// Create Cloud Run service
+const cloudRunService = new gcp.cloudrun.Service("button-click-service", {
   location: "us-central1",
   template: {
     spec: {
-      containers: [
-        {
-          image: "gcr.io/your-project-id/express-cloud-app:latest",
-          envs: [{ name: "TOPIC_NAME", value: pubsubTopic.name }],
-        },
-      ],
+      containers: [{
+        image: "gcr.io/<your-project-id>/express-rate-limit",
+      }],
     },
   },
 });
 
-export const serviceUrl = cloudRunService.status.url;
+// Set IAM policy for Cloud Run to publish to Pub/Sub
+new gcp.cloudrun.IamMember("cloud-run-publisher", {
+  service: cloudRunService.name,
+  location: cloudRunService.location,
+  role: "roles/pubsub.publisher",
+  member: "serviceAccount:" + cloudRunService.defaultServiceAccount,
+});
 
